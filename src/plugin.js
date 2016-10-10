b2ba84e7b82e2efd598e99cdd4de36be2b83359a
Ivan.Orlov <ivan@morrr.com>
2016-10-10 14:48:06 +0300

    new

--- src/plugin.js
+++ src/plugin.js
@@ -3,23 +3,27 @@ const path = require('path');
 const fs = require('fs');
 const Concat = require('concat-with-sourcemaps');
 
-module.exports = function(packages, targetName){
+module.exports = function(packages, targetName) {
    let targetFile;
    var contents2write = [];
+
    function bufferContents(file, enc, cb) {
       var fname = file.path.replace(file.base, '');
+      if (fname[0] === "/") {
+         fname = fname.slice(1)
+      }
       for (let i = 0; i < packages.length; i++) {
          let name = packages[i];
          if (fname.indexOf(name) === 0) {
             contents2write.push(`require("./${fname}");`);
          }
       }
-      fname === targetName ? targetFile = file :  this.push(file);
+      fname === targetName ? targetFile = file : this.push(file);
       cb()
    }
 
    function endStream(cb) {
-      if( targetFile ){
+      if (targetFile) {
          let concat = new Concat(true, '', '\n');
          concat.add(null, contents2write.join('\n'));
          concat.add(targetFile.path, targetFile.contents, targetFile.sourceMap);
